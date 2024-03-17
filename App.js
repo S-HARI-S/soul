766005e680e21b74d6c1c54eb4a95da3222f677e
S-HARI-S <92015765+S-HARI-S@users.noreply.github.com>
2024-03-17 20:51:27 +0530

    initial

--- App.js
+++ App.js
@@ -1,20 +1,30 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { ScrollView, StyleSheet, Text, View, TextInput } from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <ScrollView contentContainerStyle={styles.container}>
+      <TextInput
+        style={styles.textarea}
+        placeholder="Type here to translate!"
+      />
       <StatusBar style="auto" />
-    </View>
+    </ScrollView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
+  textarea: {
+    borderWidth: 5, // No need for "px" here
+    borderColor: "#000",
+    width: "80%", // Example width
+    height: 50,
+    padding:7, // Example height
+  }
 });
