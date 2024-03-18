9388aba08a767ac50d19b187424a32af5e53e37a
S-HARI-S <92015765+S-HARI-S@users.noreply.github.com>
2024-03-19 00:38:31 +0530

    refactor: added props and improved code structure

--- App.js
+++ App.js
@@ -3,7 +3,28 @@ import { ScrollView, StyleSheet, Text, View, TextInput } from "react-native";
 import Chat from "./src/chat";
 
 export default function App() {
-  return <Chat />;
+  return (
+    <ScrollView contentContainerStyle={styles.container}>
+  <Chat message = {{name:'Robert Jack', subtext: 'It came to my notice that, you arent...'}} newMessage={false} notif = {{newMessageTime:'4:30pm', default:"Yesterday", incoming:false}} />
+  <Chat message = {{name:'Rahul', subtext: 'Hey wassup? I was wondering if you could ..'}} newMessage={false} notif = {{newMessageTime:'4:30pm', default:"Yesterday", incoming:false}} />
+  <Chat message = {{name:'Robert Jack', subtext: 'lol that;s just insane'}} newMessage={true} notif = {{newMessageTime:'4:30pm', default:"Yesterday", incoming:true}} />
+  <Chat message = {{name:'Robert Jack', subtext: 'sup bro?'}} newMessage={true} notif = {{newMessageTime:'11:00am', default:"Yesterday", incoming:true}} />
+
+
+
+  
+  </ScrollView>
+  );
 }
+
+const styles = StyleSheet.create({
+
+  container: {
+    flex: 1,
+    backgroundColor: "#000",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+})
 
 
--- src/chat.js
+++ src/chat.js
@@ -1,47 +1,68 @@
-import { ScrollView, StyleSheet, Text, View, TextInput } from "react-native";
+import {
+  ScrollView,
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  TouchableHighlight,
+  Alert,
+  Vibration,
+} from "react-native";
+import React, { useCallback } from "react";
 
-export default function Chat() {
+export default function Chat({ message, notif, newMessage }) {
   return (
-    <ScrollView contentContainerStyle={styles.container}>
-
+    <TouchableHighlight
+      onLongPress={() => {
+        Vibration.vibrate(20);
+        alert("thyr");
+      }}
+    >
       <View style={styles.individualchats}>
         <View style={styles.profilePicture}></View>
-        <View style={{ width:"100%", height:50}}>
-          <View style={styles.textContent}> 
-          <Text style={styles.titleTexts}>Robert jack</Text>
-          <Text style={styles.notificationText}>4:15pm</Text>
+        <View style={{ flex: 1 }}>
+          <View style={styles.textContent}>
+            <Text style={styles.titleTexts}>{message.name}</Text>
+            <Text
+              style={
+                newMessage
+                  ?styles.notificationText 
+                  : { color: "#7B7B7B", fontSize: 12, fontWeight: "normal" }
+              }
+            >
+              {newMessage? notif.newMessageTime:notif.default}
+            </Text>
           </View>
           <View style={styles.textContent}>
-          <Text style={styles.subTexts}>tf bitch im not gay you are lol...</Text>
-          <View style={styles.notificationCircle}>
-            <Text style={{fontFamily:"Roboto", fontSize:10, fontWeight:900}}>1</Text>
+            <Text style={styles.subTexts}>{message.subtext}</Text>
+            <View
+              style={[
+                styles.notificationCircle,
+                { display: notif.incoming ? "flex" : "none" },
+              ]}
+            >
+              <Text
+                style={{ fontFamily: "Roboto", fontSize: 10, fontWeight: 900 }}
+              >
+                1
+              </Text>
+            </View>
           </View>
-          </View>
-          
         </View>
       </View>
-
-      
-      
-    </ScrollView>
+    </TouchableHighlight>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#000",
-    alignItems: "center",
-    justifyContent: "center",
-  },
   individualchats: {
+    // backgroundColor:"#fff",
     flexDirection: "row",
     alignItems: "center",
     borderWidth: 2,
-    marginVertical:10,
-    // borderColor: "#fff",
-    width: "100%",
-    height: 64, 
+    marginVertical: 10,
+    width: "98%",
+    // Remove fixed height
   },
   profilePicture: {
     height: 60,
@@ -49,13 +70,12 @@ const styles = StyleSheet.create({
     borderWidth: 2,
     borderRadius: 100,
     borderColor: "#fff",
-    marginRight: 10, 
+    marginRight: 10,
   },
   textContent: {
-    width:"80%",
     flex: 1,
-    justifyContent:"space-between",
-    flexDirection:"row",
+    justifyContent: "space-between",
+    flexDirection: "row",
   },
   titleTexts: {
     color: "#fff",
@@ -63,22 +83,22 @@ const styles = StyleSheet.create({
     fontFamily: "Roboto",
     fontWeight: "700",
   },
-  subTexts:{
-    color:"#7B7B7B",
-    fontFamily:"Roboto"
+  subTexts: {
+    color: "#7B7B7B",
+    fontFamily: "Roboto",
   },
-  notificationText:{
+  notificationText: {
     color: "#F1E5FF",
-    fontSize:12,
-    fontWeight:"700"
+    fontSize: 12,
+    fontWeight: "700",
   },
-  notificationCircle:{
-    borderRadius:50,
-    justifyContent:"center",
-    alignItems:"center",
-    height:17,
-    width:17,
+  notificationCircle: {
+    borderRadius: 50,
+    justifyContent: "center",
+    alignItems: "center",
+    height: 20,
+    width: 20,
     backgroundColor: "#F1E5FF",
-    color:"#000"
-  }
+    color: "#000",
+  },
 });
